a2522458f8bebe0b99b9db950058f56189528416
jinseongkim <kerow1004@gmail.com>
2021-12-09 10:09:54 +0900

    file edit

--- hwaseong_openapi_url.py
+++ hwaseong_openapi_url.py
@@ -30,8 +30,8 @@ class hwaseong_data(Resource):
 
             r = [dict((cur.description[i][0], value)
                       for i, value in enumerate(row)) for row in cur.fetchall()]
-            print(json.dumps(r, indent="\t", ensure_ascii=False))
-            # print(r)
+            # print(json.dumps(r, indent="\t", ensure_ascii=False))
+            print("검색성")
             return jsonify({'hwaseongDATA' : r})
         except Exception as e:
             return {'error': str(e)}
@@ -39,7 +39,7 @@ class hwaseong_data(Resource):
 from flask import Flask
 from flask_restful import Api
 
-app = Flask('My First App')
+app = Flask('hwaseong_api')
 api = Api(app)
 app.config['JSON_AS_ASCII'] = False
 api.add_resource(hwaseong_data, '/hwaseong_data')
@@ -55,4 +55,4 @@ app.config['MYSQL_DATABASE_HOST'] = '3.37.219.7'
 mysql.init_app(app)
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
