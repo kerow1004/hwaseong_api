8e877dab337f6593fa8f5cad516ddf09716e3560
jinseongkim <kerow1004@gmail.com>
2021-12-13 10:48:25 +0900

    code edit

--- hwaseong_openapi_url.py
+++ hwaseong_openapi_url.py
@@ -4,21 +4,21 @@ import flask.scaffold
 flask.helpers._endpoint_from_view_func = flask.scaffold._endpoint_from_view_func
 from flask_restful import Resource
 from flask_restful import reqparse
-import json
+import json, time
 
 mysql = MySQL()
+start = time.time()
+def timecount():
 
-
+    return (time.time() - start)
 
 
 class hwaseong_KSIC(Resource):
     def get(self):
         try:
             parser = reqparse.RequestParser()
-            parser.add_argument('KSIC', required=True, type=str)
-            parser.add_argument('HsCode', required=True, type=str)
-            parser.add_argument('NTS', required=True, type=str)
-            parser.add_argument('IndexKor', required=True, type=str)
+            parser.add_argument('ksic', required=True, type=str)
+            parser.add_argument('year', required=True, type=str)
             args = parser.parse_args()
             cur = mysql.connect().cursor()
             cur.execute('''select a.KSIC, a.IndexKor, a.IndexEng, c.HsCode, a.HsCodeKor, a.HsCodeEng, a.NTS, a.NTSKor
@@ -26,12 +26,12 @@ class hwaseong_KSIC(Resource):
                     , c.DE, c.Division, c.Profit, c.Price, c.Kg as T, c.CNT from codezip as a
                     left join ksic_Prod_DLVY as b on a.KSIC = b.KSIC and a.IndexKor = b.IndexKor
                     left join merge_country as c on a.HsCode = c.HsCode
-                    where a.KSIC = '''+args['KSIC']+'''  and c.HsCode = '''+ args['HsCode'] +''' and a.NTS = ''' +args['NTS']+ ''' and a.IndexKor like '% '''+args['IndexKor']+''' %' ''')
+                    where a.KSIC = '''+args['ksic']+''' and b.Year = ''' +args['year'] )
 
             r = [dict((cur.description[i][0], value)
                       for i, value in enumerate(row)) for row in cur.fetchall()]
             # print(json.dumps(r, indent="\t", ensure_ascii=False))
-            print("검색성공")
+            print("검색성공", timecount())
             return jsonify({'hwaseongDATA' : r})
         except Exception as e:
             return {'error': str(e)}
@@ -40,18 +40,69 @@ class hwaseong_IndexKor(Resource):
     def get(self):
         try:
             parser = reqparse.RequestParser()
-            parser.add_argument('KSIC', required=True, type=str)
-            parser.add_argument('HsCode', required=True, type=str)
-            parser.add_argument('NTS', required=True, type=str)
-            parser.add_argument('IndexKor', required=True, type=str)
+            parser.add_argument('indexkor', required=True, type=str)
+            parser.add_argument('year', required=True, type=str)
             args = parser.parse_args()
             cur = mysql.connect().cursor()
-            cur.execute('''select * from codezip''')
+            cur.execute('''select a.KSIC, a.IndexKor, a.IndexEng, c.HsCode, a.HsCodeKor, a.HsCodeEng, a.NTS, a.NTSKor
+                    , b.Year, c.Month, b.Class, b.Biz, b.Prod, b.DLVY, b.BL
+                    , c.DE, c.Division, c.Profit, c.Price, c.Kg as T, c.CNT from codezip as a
+                    left join ksic_Prod_DLVY as b on a.KSIC = b.KSIC and a.IndexKor = b.IndexKor
+                    left join merge_country as c on a.HsCode = c.HsCode
+                    where a.IndexKor like '% '''+args['indexkor']+''' %'  and b.Year = ''' +args['year'])
 
             r = [dict((cur.description[i][0], value)
                       for i, value in enumerate(row)) for row in cur.fetchall()]
             # print(json.dumps(r, indent="\t", ensure_ascii=False))
-            print("검색성공")
+            print("검색성공", timecount())
+            return jsonify({'hwaseongDATA' : r})
+        except Exception as e:
+            return {'error': str(e)}
+
+class hwaseong_Hscode(Resource):
+    def get(self):
+        try:
+            parser = reqparse.RequestParser()
+            parser.add_argument('hscode', required=True, type=str)
+            parser.add_argument('year', required=True, type=str)
+            parser.add_argument('division', required=True, type=str)
+            args = parser.parse_args()
+            cur = mysql.connect().cursor()
+            cur.execute('''select a.KSIC, a.IndexKor, a.IndexEng, c.HsCode, a.HsCodeKor, a.HsCodeEng, a.NTS, a.NTSKor
+                    , b.Year, c.Month, b.Class, b.Biz, b.Prod, b.DLVY, b.BL
+                    , c.DE, c.Division, c.Profit, c.Price, c.Kg as T, c.CNT from codezip as a
+                    left join ksic_Prod_DLVY as b on a.KSIC = b.KSIC and a.IndexKor = b.IndexKor
+                    left join merge_country as c on a.HsCode = c.HsCode
+                    where c.HsCode = ''' +args['hscode'] + ''' and b.Year = ''' +args['year'] + ''' and c.Division like "'''+args['division']+'''" ''' )
+
+
+            r = [dict((cur.description[i][0], value)
+                      for i, value in enumerate(row)) for row in cur.fetchall()]
+            print(json.dumps(r, indent="\t", ensure_ascii=False))
+            print("검색성공", timecount())
+            return jsonify({'hwaseongDATA' : r})
+        except Exception as e:
+            return {'error': str(e)}
+
+class hwaseong_NTS(Resource):
+    def get(self):
+        try:
+            parser = reqparse.RequestParser()
+            parser.add_argument('nts', required=True, type=str)
+            parser.add_argument('year', required=True, type=str)
+            args = parser.parse_args()
+            cur = mysql.connect().cursor()
+            cur.execute('''select a.KSIC, a.IndexKor, a.IndexEng, c.HsCode, a.HsCodeKor, a.HsCodeEng, a.NTS, a.NTSKor
+                    , b.Year, c.Month, b.Class, b.Biz, b.Prod, b.DLVY, b.BL
+                    , c.DE, c.Division, c.Profit, c.Price, c.Kg as T, c.CNT from codezip as a
+                    left join ksic_Prod_DLVY as b on a.KSIC = b.KSIC and a.IndexKor = b.IndexKor
+                    left join merge_country as c on a.HsCode = c.HsCode
+                    where a.NTS = '''+args['nts']+''' and b.Year = ''' +args['year'])
+
+            r = [dict((cur.description[i][0], value)
+                      for i, value in enumerate(row)) for row in cur.fetchall()]
+            # print(json.dumps(r, indent="\t", ensure_ascii=False))
+            print("검색성공", timecount())
             return jsonify({'hwaseongDATA' : r})
         except Exception as e:
             return {'error': str(e)}
@@ -62,8 +113,11 @@ from flask_restful import Api
 app = Flask('hwaseong_api')
 api = Api(app)
 app.config['JSON_AS_ASCII'] = False
-api.add_resource(hwaseong_KSIC, '/hwaseong_data/KSIC')
-api.add_resource(hwaseong_IndexKor, '/hwaseong_data/IndexKor')
+
+api.add_resource(hwaseong_KSIC, '/hwaseong_data/ksic')
+api.add_resource(hwaseong_IndexKor, '/hwaseong_data/indexkor')
+api.add_resource(hwaseong_Hscode, '/hwaseong_data/hscode')
+api.add_resource(hwaseong_NTS, '/hwaseong_data/nts')
 
 
 # MySQL configurations
